863bca7b987f2fd6ef3a9faf71217656fe5271b4
Tj Mohammed <tj@callercore.com>
2026-09-22 15:30:27 -0700

    Use centralized isolated KV boundary

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -1,5 +1,5 @@
 const crypto=require('crypto');
-const {kv}=require('@vercel/kv');
+const {kv}=require('../lib/kv');
 const {sendMail}=require('../lib/mail');
 const {safeError}=require('../lib/safe-log');
 const {lifecycleEmail}=require('../lib/email-template');
